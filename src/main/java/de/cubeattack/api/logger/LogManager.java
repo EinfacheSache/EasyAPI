e4d7ce9325722849c1ea3ecb663d343746565d53
CubeAttack <klugetommy@web.de>
2023-06-07 01:39:37 +0200

    downgrade java 17 to 8

--- src/main/java/de/cubeattack/api/logger/LogManager.java
+++ src/main/java/de/cubeattack/api/logger/LogManager.java
@@ -7,6 +7,22 @@ import org.slf4j.LoggerFactory;
 @SuppressWarnings("unused")
 public class LogManager
 {
+
+    static {
+        String version = System.getProperty("java.version");
+        if(version.startsWith("1.")) {
+            version = version.substring(2, 3);
+        } else {
+            int dot = version.indexOf(".");
+            if(dot != -1) { version = version.substring(0, dot); }
+        }
+        if(Integer.parseInt(version) < 11){
+            System.setProperty("logback.configurationFile", "src/main/resources/logback-8.xml");
+        }else {
+            System.setProperty("logback.configurationFile", "src/main/resources/logback-17.xml");
+        }
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger("de.cubeattack");
 
     public static void setLevel(Level level) {
