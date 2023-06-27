4e10dee4bc549cd19fb732cc3ddc49c18bc01df5
EinfacheSache <klugetommy@web.de>
2023-06-27 16:03:36 +0200

    rework LogManager

--- src/main/java/de/cubeattack/api/logger/CustomClassOfCallerConverter.java
+++ src/main/java/de/cubeattack/api/logger/CustomClassOfCallerConverter.java
@@ -10,10 +10,7 @@ public class CustomClassOfCallerConverter extends ClassOfCallerConverter {
     public String convert(ILoggingEvent event) {
         StackTraceElement[] stackTrace = event.getCallerData();
         if (stackTrace != null && stackTrace.length > 0) {
-
-            System.out.println(Arrays.asList(stackTrace));
-
-            StackTraceElement element = Arrays.asList(stackTrace).get(stackTrace.length-event.getCallerData().length == 2 ? 1 : 2);
+            StackTraceElement element = Arrays.asList(stackTrace).get(stackTrace.length - (event.getCallerData().length <= 2 ? 1 : 2));
             return element.getClassName() + ":" + element.getMethodName();
         }
         return super.convert(event);
