cd1c413d19e663d00495b5d2d25b42410f615f73
EinfacheSache <klugetommy@web.de>
2023-08-05 02:45:58 +0200

    Code -> API now much smaller

--- src/main/java/de/cubeattack/api/logger/LogManager.java
+++ src/main/java/de/cubeattack/api/logger/LogManager.java
@@ -1,12 +1,11 @@
 package de.cubeattack.api.logger;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import de.cubeattack.api.util.JavaUtils;
 import org.slf4j.LoggerFactory;
 
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
 public class LogManager
@@ -33,34 +32,36 @@ public class LogManager
     }
 
     public LogManager setLevel(Object level, Object logger) {
-        if(logger instanceof Logger && level instanceof Level) ((Logger) logger).setLevel((Level) level);
+        if(logger instanceof ch.qos.logback.classic.Logger && level instanceof ch.qos.logback.classic.Level) ((ch.qos.logback.classic.Logger) logger).setLevel((ch.qos.logback.classic.Level) level);
         if(logger instanceof java.util.logging.Logger && level instanceof java.util.logging.Level) ((java.util.logging.Logger) logger).setLevel((java.util.logging.Level) level);
         return this;
     }
 
     public void trace(String output){
-        if(logger instanceof Logger) ((Logger) logger).trace(output);
+        if(logger instanceof org.slf4j.Logger) ((org.slf4j.Logger) logger).trace(output);
         if(logger instanceof java.util.logging.Logger) ((java.util.logging.Logger) logger).log(java.util.logging.Level.FINE, output);
     }
     public void debug(String output){
-        if(logger instanceof Logger) ((Logger) logger).debug(output);
+        if(logger instanceof org.slf4j.Logger) ((org.slf4j.Logger) logger).debug(output);
         if(logger instanceof java.util.logging.Logger) ((java.util.logging.Logger) logger).log(java.util.logging.Level.CONFIG, output);
     }
 
     public void info(String output){
-        if(logger instanceof Logger) ((Logger) logger).info(output);
-        if(logger instanceof java.util.logging.Logger) ((java.util.logging.Logger) logger).log(java.util.logging.Level.INFO, output);
+        if(logger instanceof Logger){
+            ((Logger) logger).log(java.util.logging.Level.INFO, output);
+        }else
+            if(logger instanceof org.slf4j.Logger) ((org.slf4j.Logger) logger).info(output);
     }
     public void warn(String output){
-        if(logger instanceof Logger) ((Logger) logger).warn(output);
+        if(logger instanceof org.slf4j.Logger) ((org.slf4j.Logger) logger).warn(output);
         if(logger instanceof java.util.logging.Logger) ((java.util.logging.Logger) logger).log(java.util.logging.Level.WARNING, output);
     }
     public void error(String output){
-        if(logger instanceof Logger) ((Logger) logger).error(output);
+        if(logger instanceof org.slf4j.Logger) ((org.slf4j.Logger) logger).error(output);
         if(logger instanceof java.util.logging.Logger) ((java.util.logging.Logger) logger).log(java.util.logging.Level.SEVERE, output);
     }
     public void error(String output, Throwable throwable){
-        if(logger instanceof Logger) ((Logger) logger).error(output, throwable);
+        if(logger instanceof org.slf4j.Logger) ((org.slf4j.Logger) logger).error(output, throwable);
         if(logger instanceof java.util.logging.Logger) ((java.util.logging.Logger) logger).log(java.util.logging.Level.SEVERE, output, throwable);
     }
 
@@ -70,19 +71,19 @@ public class LogManager
         Handler handler = new Handler() {
             @Override
             public void publish(LogRecord record) {
-                Level level;
+                ch.qos.logback.classic.Level level;
                 if (record.getLevel() == java.util.logging.Level.SEVERE) {
-                    level = Level.ERROR;
+                    level = ch.qos.logback.classic.Level.ERROR;
                 } else if (record.getLevel() == java.util.logging.Level.WARNING) {
-                    level = Level.WARN;
+                    level = ch.qos.logback.classic.Level.WARN;
                 } else if (record.getLevel() == java.util.logging.Level.INFO) {
-                    level = Level.INFO;
+                    level = ch.qos.logback.classic.Level.INFO;
                 } else {
-                    level = Level.DEBUG;
+                    level = ch.qos.logback.classic.Level.DEBUG;
                 }
 
-                if(!(logger instanceof Logger))return;
-                ((Logger)logger).log(null, Logger.FQCN, level.toInt()/1000, record.getMessage(), null, record.getThrown());
+                if(!(logger instanceof ch.qos.logback.classic.Logger))return;
+                ((ch.qos.logback.classic.Logger)logger).log(null, ch.qos.logback.classic.Logger.FQCN, level.toInt()/1000, record.getMessage(), null, record.getThrown());
             }
 
             @Override
