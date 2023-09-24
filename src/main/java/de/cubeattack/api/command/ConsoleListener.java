ea985bbcd0e6f0ea21389fa6ab53e21bd4b459e2
EinfacheSache <klugetommy@web.de>
2023-09-24 11:14:36 +0200

    Testing -> Added Test listener

--- src/main/java/de/cubeattack/api/command/ConsoleListener.java
+++ src/main/java/de/cubeattack/api/command/ConsoleListener.java
@@ -1,10 +1,14 @@
 package de.cubeattack.api.command;
 
 import de.cubeattack.api.API;
-import de.cubeattack.api.logger.LogManager;
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +20,10 @@ public class ConsoleListener
         run();
     }
 
+    public static void main(String[] args) {
+        //new ConsoleListener().registerCommand(args1 -> System.out.println("Ausgabe"), "test");
+        //new ConsoleListener().registerCommand(args1 -> System.out.println("test1"), "test");
+    }
 
     private final List<ConsoleCommand> commands = new ArrayList<>();
 
@@ -26,11 +34,53 @@ public class ConsoleListener
 
     private void run() {
         API.getExecutorService().submit(() -> {
+                    try {
+                        // Erstelle ein Terminal
+                        Terminal terminal = TerminalBuilder.builder()
+                                .system(true)
+                                .build();
+
+                        // Erstelle einen LineReader für die Eingabe
+                        LineReader reader = LineReaderBuilder.builder()
+                                .terminal(terminal)
+                                .build();
+
+                        // Schleife zum Lesen von Eingaben
+                        String prompt = "> ";
+                        while (true) {
+                            String line;
+                            try {
+                                line = reader.readLine(prompt);
+                            } catch (UserInterruptException e) {
+                                // Benutzer hat Strg+C gedrückt
+                                continue;
+                            } catch (EndOfFileException e) {
+                                // Benutzer hat Strg+D gedrückt oder die Eingabe wurde beendet
+                                break;
+                            }
+                            if (line.equalsIgnoreCase("Hallo")) {
+                                System.out.println("Hey wie geht es dir?");
+                                continue;
+                            }
+
+                            // Hier kannst du die eingegebene Zeile verarbeiten
+                            System.out.println("Eingabe: " + line);
+                        }
+
+                        // Beende das Terminal
+                        terminal.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        /*
+        API.getExecutorService().submit(() -> {
             String line;
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            //Scanner scanner = new Scanner(System.in);
             try {
-                while ((line = reader.readLine()) != null) {
+
+                while ( (line = reader.readLine()) != null) {
                     String cmd = line.toLowerCase().split(" ")[0];
                     String[] args = line.replace(line.split(" ")[0] + " ", "").split(" ");
 
@@ -51,5 +101,6 @@ public class ConsoleListener
                 LogManager.getLogger().error("Error whiles reading command : " + ex.getLocalizedMessage());
             }
         });
+         */
     }
 }
---
+++ src/main/java/de/cubeattack/api/command/ConsoleReaderExample.java
@@ -0,0 +1,58 @@
+package de.cubeattack.api.command;
+
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+import java.io.IOException;
+
+public class ConsoleReaderExample {
+
+
+    public static void main(String[] args) {
+
+        //new ConsoleListener().registerCommand(args1 -> System.out.println("Ausgabe"), "test");
+
+        try {
+            // Erstelle ein Terminal
+            Terminal terminal = TerminalBuilder.builder()
+                    .system(true)
+                    .build();
+
+            // Erstelle einen LineReader für die Eingabe
+            LineReader reader = LineReaderBuilder.builder()
+                    .terminal(terminal)
+                    .build();
+
+            // Schleife zum Lesen von Eingaben
+            String prompt = "> ";
+            while (true) {
+                String line;
+                try {
+                    line = reader.readLine(prompt);
+                } catch (UserInterruptException e) {
+                    // Benutzer hat Strg+C gedrückt
+                    continue;
+                } catch (EndOfFileException e) {
+                    // Benutzer hat Strg+D gedrückt oder die Eingabe wurde beendet
+                    break;
+                }
+                if(line.equalsIgnoreCase("Hallo")){
+                    System.out.println("Hey wie geht es dir?");
+                    continue;
+                }
+
+                // Hier kannst du die eingegebene Zeile verarbeiten
+                System.out.println("Eingabe: " + line);
+            }
+
+            // Beende das Terminal
+            terminal.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
