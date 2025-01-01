c33d3e959f7b830588ed8d28e948b46fdb380bf6
EinfacheSache <klugetommy@web.de>
2025-01-01 03:53:37 +0100

    Changes -> Added StatsUpdateSchedule

---
+++ src/main/java/de/cubeattack/api/minecraft/stats/Stats.java
@@ -0,0 +1,112 @@
+package de.cubeattack.api.minecraft.stats;
+
+public class Stats {
+
+    private final String serverType;
+    private final String serverVersion;
+    private final String serverName;
+    private final String javaVersion;
+    private final String osName;
+    private final String osArch;
+    private final String osVersion;
+    private final String pluginVersion;
+    private final String versionStatus;
+    private final String versionError;
+    private final String updateSetting;
+    private final String neoProtectPlan;
+
+    private final int playerAmount;
+    private final int managedServers;
+    private final int coreCount;
+
+    private final boolean onlineMode;
+    private final boolean proxyProtocol;
+
+    public Stats(String serverType, String serverVersion, String serverName, String javaVersion, String osName, String osArch, String osVersion, String pluginVersion, String versionStatus, String versionError, String updateSetting, String neoProtectPlan, int playerAmount, int managedServers, int coreCount, boolean onlineMode, boolean proxyProtocol) {
+        this.serverType = serverType;
+        this.serverVersion = serverVersion;
+        this.serverName = serverName;
+        this.javaVersion = javaVersion;
+        this.osName = osName;
+        this.osArch = osArch;
+        this.osVersion = osVersion;
+        this.pluginVersion = pluginVersion;
+        this.versionStatus = versionStatus;
+        this.versionError = versionError;
+        this.updateSetting = updateSetting;
+        this.neoProtectPlan = neoProtectPlan;
+        this.playerAmount = playerAmount;
+        this.managedServers = managedServers;
+        this.coreCount = coreCount;
+        this.onlineMode = onlineMode;
+        this.proxyProtocol = proxyProtocol;
+    }
+
+    public String getServerType() {
+        return serverType;
+    }
+
+    public String getServerVersion() {
+        return serverVersion;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public String getJavaVersion() {
+        return javaVersion;
+    }
+
+    public String getOsName() {
+        return osName;
+    }
+
+    public String getOsArch() {
+        return osArch;
+    }
+
+    public String getOsVersion() {
+        return osVersion;
+    }
+
+    public String getPluginVersion() {
+        return pluginVersion;
+    }
+
+    public String getVersionStatus() {
+        return versionStatus;
+    }
+
+    public String getVersionError() {
+        return versionError;
+    }
+
+    public String getUpdateSetting() {
+        return updateSetting;
+    }
+
+    public String getNeoProtectPlan() {
+        return neoProtectPlan;
+    }
+
+    public int getPlayerAmount() {
+        return playerAmount;
+    }
+
+    public int getManagedServers() {
+        return managedServers;
+    }
+
+    public int getCoreCount() {
+        return coreCount;
+    }
+
+    public boolean isOnlineMode() {
+        return onlineMode;
+    }
+
+    public boolean isProxyProtocol() {
+        return proxyProtocol;
+    }
+}
---
+++ src/main/java/de/cubeattack/api/minecraft/stats/StatsManager.java
@@ -0,0 +1,52 @@
+package de.cubeattack.api.minecraft.stats;
+
+import com.google.gson.Gson;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import de.cubeattack.api.logger.LogManager;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
+public class StatsManager {
+
+
+    private final OkHttpClient client = new OkHttpClient();
+    private final String statsServer = "https://metrics.einfachesache.de/api/stats/plugin";
+
+
+    private void runStatsUpdateSchedule(String ID, String address, Stats stats, int updatePeriodInSec) {
+
+        LogManager.getLogger().info("StatsUpdate scheduler started");
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), new Gson().toJson(stats));
+                if(!updateStats(requestBody, String.valueOf(UUID.nameUUIDFromBytes((ID + ":" + address).getBytes(StandardCharsets.UTF_8)))))
+                    LogManager.getLogger().info("Request to update stats failed");
+                else
+                    LogManager.getLogger().debug("Request to update stats was successful");
+            }
+        }, 1000, updatePeriodInSec * 5L);
+    }
+
+    public boolean updateStats(RequestBody requestBody, String identifier) {
+        try {
+            return client.newCall(new Request.Builder()
+                    .url(statsServer)
+                    .addHeader("accept", "*/*")
+                    .addHeader("Content-Type", "application/json")
+                    .header("identifier", identifier)
+                    .post(requestBody).build()).execute().code() == 200;
+        }catch (IOException exception) {
+            return false;
+        }
+    }
+}
