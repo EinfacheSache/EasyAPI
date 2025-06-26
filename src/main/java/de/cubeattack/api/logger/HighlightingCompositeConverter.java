9c4cf6c08b7d4a6798199a5b7f5837bf2f1bd681
einfachesache <klugetommy@web.de>
2025-06-26 20:27:47 +0200

    changes -> dependency updating + testing

--- src/main/java/de/cubeattack/api/logger/HighlightingCompositeConverter.java
+++ src/main/java/de/cubeattack/api/logger/HighlightingCompositeConverter.java
@@ -11,13 +11,13 @@ public class HighlightingCompositeConverter extends ForegroundCompositeConverter
     @Override
     protected String getForegroundColorCode(ILoggingEvent event) {
         Level level = event.getLevel();
-        switch (level.toInt()) {
-            case Level.ERROR_INT : return RED_FG;
-            case Level.WARN_INT : return YELLOW_FG;
-            case Level.INFO_INT : return BLUE_FG;
-            case Level.DEBUG_INT : return GREEN_FG;
-            case Level.TRACE_INT : return CYAN_FG;
-            default : throw new IllegalStateException("Unexpected value: " + level.toInt());
-        }
+        return switch (level.toInt()) {
+            case Level.ERROR_INT -> RED_FG;
+            case Level.WARN_INT -> YELLOW_FG;
+            case Level.INFO_INT -> BLUE_FG;
+            case Level.DEBUG_INT -> GREEN_FG;
+            case Level.TRACE_INT -> CYAN_FG;
+            default -> throw new IllegalStateException("Unexpected value: " + level.toInt());
+        };
     }
 }
