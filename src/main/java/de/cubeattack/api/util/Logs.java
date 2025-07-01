0523a80e0ad26eebce6668837b87084b1f76661c
einfachesache <klugetommy@web.de>
2025-07-01 23:25:17 +0200

    changes -> added file logging

---
+++ src/main/java/de/cubeattack/api/util/Logs.java
@@ -0,0 +1,25 @@
+package de.cubeattack.api.util;
+
+import de.cubeattack.api.logger.LogManager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+@SuppressWarnings("unused")
+public class Logs {
+
+    public static void write(Path path,String message) {
+        try {
+            Files.writeString(
+                    path,
+                    message + "\n",
+                    StandardOpenOption.APPEND,
+                    StandardOpenOption.CREATE
+            );
+        } catch (IOException e) {
+            LogManager.getLogger().error("Error writing to log file", e);
+        }
+    }
+}
